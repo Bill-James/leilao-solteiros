c3d19db2a5cca3e44de8c57db31b4ce3b97454ca
Henrique Jaime <153871379+Bill-James@users.noreply.github.com>
2025-11-27 01:43:22 +0100

    Update Firebase configuration with new credentials

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY_AQUI",
+  apiKey: "AIzaSyDEYpuxtgQvLdoGNHjIp6v8R31cQXPfk70",
   authDomain: "leil-4af40.firebaseapp.com",
   databaseURL: "https://leil-4af40-default-rtdb.firebaseio.com",
   projectId: "leil-4af40",
-  storageBucket: "leil-4af40.appspot.com",
-  messagingSenderId: "SEU_ID",
-  appId: "SEU_APP_ID"
+  storageBucket: "leil-4af40.firebasestorage.app",
+  messagingSenderId: "779447652746",
+  appId: "1:779447652746:web:1639d62e7ad4dcac6c33c6"
 };
 
 const app = initializeApp(firebaseConfig);
