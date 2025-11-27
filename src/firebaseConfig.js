97b46cf65cc0e234ee54cac5794ed7b189a7df3a
Henrique Jaime <153871379+Bill-James@users.noreply.github.com>
2025-11-27 09:33:37 +0100

    Update Firebase config to use environment variables

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDEYpuxtgQvLdoGNHjIp6v8R31cQXPfk70",
-  authDomain: "leil-4af40.firebaseapp.com",
-  databaseURL: "https://leil-4af40-default-rtdb.firebaseio.com/",
-  projectId: "leil-4af40",
-  storageBucket: "leil-4af40.firebasestorage.app",
-  messagingSenderId: "779447652746",
-  appId: "1:779447652746:web:1639d62e7ad4dcac6c33c6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyDEYpuxtgQvLdoGNHjIp6v8R31cQXPfk70",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "leil-4af40.firebaseapp.com",
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL || "https://leil-4af40-default-rtdb.firebaseio.com/",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "leil-4af40",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "leil-4af40.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "779447652746",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:779447652746:web:1639d62e7ad4dcac6c33c6"
 };
 
 const app = initializeApp(firebaseConfig);
